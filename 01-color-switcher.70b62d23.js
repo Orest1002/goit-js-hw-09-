9b01787ca9821802112d3ca79eafd0554fbc5999
Orest1002 <118014212+Orest1002@users.noreply.github.com>
2023-04-27 17:50:30 +0000

    Deploying to gh-pages from @ Orest1002/goit-js-hw-09-@95cfa022a04c21d4e68b7c8fa81de6bb91f03b1e 🚀

---
+++ 01-color-switcher.70b62d23.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0,t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,timerId=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),d.addEventListener("click",(()=>{t.disabled=!1,d.disabled=!0,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.70b62d23.js.map
